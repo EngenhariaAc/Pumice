dfda1b94e3ee0b840e92951627d5a39c7c104701
elndower <51922927+EngenhariaAc@users.noreply.github.com>
2026-04-18 19:29:34 +0700

    Create Volcano.java

---
+++ Volcano.java
@@ -0,0 +1,11 @@
+public class Volcano {
+    private String name;
+
+    public Volcano(String name) {
+        this.name = name;
+    }
+
+    public void erupt() {
+        System.out.println(name + " is erupting! Pumice is forming!");
+    }
+}
